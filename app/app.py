36c12c14d25426d0c9af47828181b2824d671d2d
pawankumar94 <pawan.kumar@st.ovgu.de>
2020-04-01 17:00:46 +0200

    Updated the app file

--- app/app.py
+++ app/app.py
@@ -4,15 +4,17 @@ from datetime import timedelta
 import subprocess
 from werkzeug.utils import secure_filename
 import os
-import datetime
+import datetime as dt
 import urllib.request
-import uuid
+import uuid,shutil,time
+from pathlib import Path
 
-app = Flask(__name__,template_folder=r'C:\Users\Pawan\Desktop\Flask\venv\pawankumar94\app\Template')
+app = Flask(__name__,template_folder=Path(r'env\Scripts\static').resolve())
 app.permanent_session_lifetime = timedelta(minutes=1)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]iasdfffsd/'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'csv'])
-app.config['UPLOAD_FOLDER'] = r'C:\Users\Pawan\Desktop\Flask\venv\pawankumar94\app\filename'
+app.config['UPLOAD_FOLDER'] = Path(r'env\filename').resolve()
+path= Path(r'env\filename').resolve()
 
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -24,11 +26,11 @@ def home():
 
 def makefolder():
     user = request.form["name"]
-    session["user"] = user 
-    os.chdir(r'C:\Users\Pawan\Desktop\Flask\venv\pawankumar94\app\Template')
+    session["users"] = user 
+    os.chdir(path)
     newfolder = user
     os.makedirs(newfolder)
-    return newfolder
+    return newfolder 
 
 
 @app.route('/login' , methods = ["POST", "GET"])
@@ -36,7 +38,7 @@ def login():
     if request.method == "POST":
         session.permanent = True
         user = request.form["name"]
-        session["user"] = user 
+        session["users"] = user 
         return redirect(url_for("user"))
 
     return render_template('login.html')
@@ -44,10 +46,9 @@ def login():
 @app.before_request
 def before_request():
     g.user = None
-    if "user" in session:
-        g.user = session["user"]
+    if "users" in session:
+        g.user = session["users"]
         
-
 @app.route('/user' )
 def user():
     if g.user:
@@ -61,43 +62,76 @@ def user():
 def uploader():
     return render_template('upload1.html')
 
+def delete():
+    seconds= time.time() - (3600)
+    if os.path.exists(path):
+        for root_folder, folders, files in os.walk(path):
+
+            for folder in folders:
+                folder_path = os.path.join(root_folder, folder)
+                if seconds >= get_file_or_folder_age(folder_path):
+                    remove_folder(folder_path)
+                        
+                   
+    else:
+        print(f'"{path}" is not found')
+
+def remove_folder(path):
+	if not shutil.rmtree(path):
+		print(f"{path} is removed successfully")
+
+	else:
+		print(f"Unable to delete the {path}")
+
+
+def get_file_or_folder_age(path):
+	ctime = os.stat(path).st_ctime
+	return ctime
+
 
 @app.route('/upload',methods=['POST'])
 def upload():
     if g.user:
         uploaded_files = request.files.getlist("file[]")
         filenames = []
-        directory = g.user
-        path = os.path.join(app.config['UPLOAD_FOLDER'],directory)
+        path = os.path.join(app.config['UPLOAD_FOLDER'],str(uuid.uuid4().hex))
         try:
-            os.makedirs(path, exist_ok = True) 
+            os.makedirs(path, exist_ok = True)
         except OSError as error:
-            print("Directory '%s' can not be created") 
-        
+            print("Directory '%s' can not be created")
+
         for file in uploaded_files:
-            
             if file and allowed_file(file.filename):
                 filename = secure_filename(file.filename)
-
                 file.save(os.path.join(path, filename))
                 filenames.append(filename)
-            
         return render_template('upload2.html', filenames=filenames)
+                
     return redirect(url_for("login"))
     
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
-       
+
+        
     
 @app.route("/logout")
 def logout():   
-    session.pop("pawan", None)#pops out the key value of the session , if the is not present then it will print none
+    session.pop("users", None)
     return redirect(url_for("login"))
 
 
-if __name__ == "__main__":
-    	app.run(debug=True)
+
+def main():
+    delete()
+    app.debug = True
+    app.run()
     
+
+if __name__ == "__main__":
+    main()
+    
+    	
+       
 
